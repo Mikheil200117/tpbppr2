e08b3f916a15617cdd4463738fb09805ac8bcc2b
Mae-lv <70637893+Mae-lv@users.noreply.github.com>
2024-10-25 01:31:43 +0300

    Update setup.py

--- .github/workflows/setup.py
+++ .github/workflows/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tpbp_pr2',
+    name='tpbppr2',
     version='1.0.0',           
     packages=find_packages(),
     install_requires=[
